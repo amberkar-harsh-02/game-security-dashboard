b6084658d42d52bfae7c5eea4e4a50f0bc288fce
amberkar-harsh-02 <harshamberkar02@gmail.com>
2025-10-23 10:10:56 -0700

    Updated my backend for CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import json
 
 # Initialize the Flask application
 app = Flask(__name__)
+CORS(app)
 
 # --- DATABASE CONFIGURATION ---
 # The default username is 'postgres'
